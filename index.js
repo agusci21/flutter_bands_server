35f92e245b4b0b9e2ba773dba5f517aa9e1ed89c
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-09-19 09:24:21 -0300

    Puertos dinamicos

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+require('dotenv').config();
+
+const app = express();
 
 const publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
 
-app.listen(3000,(err)=>{
+app.listen(process.env.PORT,(err)=>{
     if(err)throw new Error(err);
 
-    console.log('Servido corriendo en puerto ', 3000);
+    console.log('Servido corriendo en puerto ', process.env.PORT);
 });
