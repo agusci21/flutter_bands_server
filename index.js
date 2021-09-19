9f60111ee4c7cdd92d2165f26933c1dbb6cc7575
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-09-19 10:33:31 -0300

    Server finalizado

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-//App de express
+
+// App de Express
 const app = express();
-//Servidor de Node
+
+// Node Server
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
 
-//Mensajes de Sockets
 
-io.on('connection', client => {
 
-    console.log('Cliente conectado');
 
-    client.on('disconnect', () => { 
-        console.log('Cliente desconectado')
-     });
+// Path público
+const publicPath = path.resolve( __dirname, 'public' );
+app.use( express.static( publicPath ) );
+
+
+
+
+
+server.listen( process.env.PORT, ( err ) => {
+
+    if ( err ) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto', process.env.PORT );
+
 });
 
 
-//path publico
-
-
-const publicPath = path.resolve(__dirname, 'public');
-
-app.use(express.static(publicPath));
-
-server.listen(process.env.PORT,(err)=>{
-    if(err)throw new Error(err);
-
-    console.log('Servido corriendo en puerto ', process.env.PORT);
-});
