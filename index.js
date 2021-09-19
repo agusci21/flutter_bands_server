37db61ff4e49cd80ccc182eb34c7d6f91f991e83
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-09-18 23:20:19 -0300

    Primer commit socket

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicPath = path.resolve(__dirname, 'public');
+
+app.use(express.static(publicPath));
+
+app.listen(3000,(err)=>{
+    if(err)throw new Error(err);
+
+    console.log('Servido corriendo en puerto ', 3000);
+});
