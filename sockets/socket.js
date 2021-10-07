93e174864044dc3a975dee7f25448c7fb86a9ad3
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-10-07 20:35:14 -0300

    emitir active bands

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,21 @@
+const Bands = require('../models/bands_model');
 const { io } = require('../index');
+const Band = require('../models/band_model');
 
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jon Jovi'));
+bands.addBand(new Band('Heroes del silencio'));
+bands.addBand(new Band('Metalica'));
+
+console.log(bands);
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
